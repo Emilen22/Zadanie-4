354db2c8afe351fe3e5af5ec22d5a555fd4698d9
Emilen <emilianknysak@gmail.com>
2019-01-17 13:16:24 +0100

    Zadanie 1.2

--- src/Telephone.java
+++ src/Telephone.java
@@ -3,4 +3,12 @@ public class Telephone {
     String model;
     String color;
     int number;
+
+    Telephone(String b, String m, String c, int n) {
+        brand = b;
+        model = m;
+        color = c;
+        number =n;
+    }
+
 }
--- src/Telephones.java
+++ src/Telephones.java
@@ -1,18 +1,18 @@
 public class Telephones {
     public static void main(String[] args) {
-        Telephone tel1 = new Telephone();
-        Telephone tel2 = new Telephone();
+        Telephone tel1 = new Telephone("Huawei", "P20", "Czarny", 123654789);
+        Telephone tel2 = new Telephone("Apple", "iPhone Xs", "Biały", 321456879);
 
-        tel1.brand = "Huawei";
-        tel1.model = "P20";
-        tel1.color = "Czarny";
-        tel1.number = 123654789;
-
-        tel2.brand = "Apple";
-        tel2.model = "iPhone Xd";
-        tel2.color = "Biały";
-        tel2.number = 321456879;
-
+//        tel1.brand = "Huawei";
+//        tel1.model = "P20";
+//        tel1.color = "Czarny";
+//        tel1.number = 123654789;
+//
+//        tel2.brand = "Apple";
+//        tel2.model = "iPhone Xs";
+//        tel2.color = "Biały";
+//        tel2.number = 321456879;
+//
         System.out.println(tel1.brand);
         System.out.println(tel1.model);
         System.out.println(tel1.color);
